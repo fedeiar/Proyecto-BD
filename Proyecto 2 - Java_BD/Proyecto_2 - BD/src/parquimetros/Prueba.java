0552bb0a8d5b7a03a62f3bc0c975f7a7305a9e7a
Federico <42450883+fedeiar@users.noreply.github.com>
2020-10-23 14:21:56 -0300

    Revert "revert"
    This reverts commit 16c986dbe9210b7e0058c3e6e0dbeca7820ba6b9.

--- Proyecto 2 - Java_BD/Proyecto_2 - BD/src/parquimetros/Prueba.java
+++ Proyecto 2 - Java_BD/Proyecto_2 - BD/src/parquimetros/Prueba.java
@@ -2,31 +2,11 @@ package parquimetros;
 
 import java.awt.EventQueue;
 
-<<<<<<< HEAD
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import java.awt.BorderLayout;
 import java.awt.Panel;
 import java.awt.TextField;
-=======
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-
-import java.awt.BorderLayout;
-import java.awt.Panel;
-import java.awt.TextField;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import java.awt.GridLayout;
-import java.awt.Label;
-import java.awt.Button;
-import java.awt.Font;
-import javax.swing.JPasswordField;
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeEvent;
->>>>>>> parent of 73b3533... FEDE DEJA DE ROBAR
 
 public class Prueba {
 
@@ -53,31 +33,12 @@ public class Prueba {
 	 */
 	public Prueba() {
 		initialize();
-<<<<<<< HEAD
-=======
-		
-		 
-
-	      
-
-	      
-	      
-	     
-
->>>>>>> parent of 73b3533... FEDE DEJA DE ROBAR
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-<<<<<<< HEAD
-=======
-		
-		
-			 
-			
->>>>>>> parent of 73b3533... FEDE DEJA DE ROBAR
 		frame = new JFrame();
 		frame.setBounds(100, 100, 641, 531);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
